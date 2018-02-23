dc827d21913c6231573e04c6ee84ff4eb3cf4850
Wang-Cheng Kang <Kwc-Oliver@Wang-Chengs-MacBook-Pro.local>
2018-02-23 14:56:12 -0800

    Update datasets

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 wget http://cseweb.ucsd.edu/~wckang/DVBPR/AmazonFashion6ImgPartitioned.npy
+#wget http://cseweb.ucsd.edu/~wckang/DVBPR/AmazonWomenWithImgPartitioned.npy
+#wget http://cseweb.ucsd.edu/~wckang/DVBPR/AmazonMenWithImgPartitioned.npy
+#wget http://cseweb.ucsd.edu/~wckang/DVBPR/TradesyImgPartitioned.npy
